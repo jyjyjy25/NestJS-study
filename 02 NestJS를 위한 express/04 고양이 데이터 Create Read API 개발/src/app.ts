9f91e68f041a200a5a6c1f3d9de07e295d89abf4
jyjyjy25 <jyjyjy7418@gmail.com>
2023-07-14 00:56:44 +0900

    2-4 고양이 데이터 Create Read API 개발

---
+++ 02 NestJS를 위한 express/04 고양이 데이터 Create Read API 개발/src/app.ts
@@ -0,0 +1,81 @@
+import * as express from "express";
+import { Cat, CatType } from "./app.model";
+
+const app: express.Express = express();
+
+//* logging middleware
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is logging middleware");
+  next();
+});
+
+//* json middleware
+app.use(express.json());
+
+//* READ 고양이 전체 데이터 모두 조회
+app.get("/cats", (req, res) => {
+  try {
+    const cats = Cat;
+    // throw new Error("db connect error");
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* READ 특정 고양이 데이터 조회
+app.get("/cats/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    const cat = Cat.find((cat) => {
+      return cat.id === id;
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* CREATE 새로운 고양이 추가
+app.post("/cats", (req, res) => {
+  try {
+    const data = req.body;
+    Cat.push(data); // create
+    res.status(200).send({
+      success: true,
+      data: { data },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* 404 middleware
+app.use((req, res, next) => {
+  console.log("this is error middleware");
+  res.send({ error: "404 not found error" });
+});
+
+app.listen(8000, () => {
+  console.log("server is on...");
+});
