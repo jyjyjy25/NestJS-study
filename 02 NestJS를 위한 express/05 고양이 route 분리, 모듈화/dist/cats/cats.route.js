6c61261c4870165be5a5db8b93837d231e64f7b4
jyjyjy25 <jyjyjy7418@gmail.com>
2023-07-14 01:05:23 +0900

    2-5 고양이 route 분리, 모듈화

---
+++ 02 NestJS를 위한 express/05 고양이 route 분리, 모듈화/dist/cats/cats.route.js
@@ -0,0 +1,60 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var cats_model_1 = require("./cats.model");
+var express_1 = require("express");
+var router = express_1.Router();
+router.get("/cats", function (req, res) {
+    try {
+        var cats = cats_model_1.Cat;
+        res.status(200).send({
+            success: true,
+            data: {
+                cats: cats,
+            },
+        });
+    }
+    catch (error) {
+        res.status(400).send({
+            success: false,
+            error: error.message,
+        });
+    }
+});
+router.get("/cats/:id", function (req, res) {
+    try {
+        var id_1 = req.params.id;
+        var cat = cats_model_1.Cat.find(function (cat) {
+            return cat.id === id_1;
+        });
+        res.status(200).send({
+            success: true,
+            data: {
+                cat: cat,
+            },
+        });
+    }
+    catch (error) {
+        res.status(400).send({
+            success: false,
+            error: error.message,
+        });
+    }
+});
+router.post("/cats", function (req, res) {
+    try {
+        var data = req.body;
+        cats_model_1.Cat.push(data);
+        res.status(200).send({
+            success: true,
+            data: { data: data },
+        });
+    }
+    catch (error) {
+        res.status(400).send({
+            success: false,
+            error: error.message,
+        });
+    }
+});
+exports.default = router;
+//# sourceMappingURL=cats.route.js.map
---
+++ 02 NestJS를 위한 express/05 고양이 route 분리, 모듈화/src/cats/cats.route.ts
@@ -0,0 +1,63 @@
+import { Cat, CatType } from "./cats.model";
+import { Router } from "express";
+
+const router = Router();
+
+//* READ 고양이 전체 데이터 모두 조회
+router.get("/cats", (req, res) => {
+  try {
+    const cats = Cat;
+    // throw new Error("db connect error");
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* READ 특정 고양이 데이터 조회
+router.get("/cats/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    const cat = Cat.find((cat) => {
+      return cat.id === id;
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* CREATE 새로운 고양이 추가
+router.post("/cats", (req, res) => {
+  try {
+    const data = req.body;
+    Cat.push(data); // create
+    res.status(200).send({
+      success: true,
+      data: { data },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+export default router;
