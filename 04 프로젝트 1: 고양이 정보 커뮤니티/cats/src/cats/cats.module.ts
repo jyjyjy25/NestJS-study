5caf001943e18976c93dec6af323a345e851fbff
jyjyjy25 <jyjyjy7418@gmail.com>
2023-08-03 00:35:04 +0900

    4-4 Repository 패턴과 레이어 분리

--- 04 프로젝트 1: 고양이 정보 커뮤니티/cats/src/cats/cats.module.ts
+++ 04 프로젝트 1: 고양이 정보 커뮤니티/cats/src/cats/cats.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
-import { CatSchema } from './cats.schema';
-import { Cat } from './cats.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Cat } from './cats.schema';
+import { CatSchema } from './cats.schema';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
+import { CatsRepository } from './cats.repository';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
   controllers: [CatsController],
-  providers: [CatsService],
+  providers: [CatsService, CatsRepository],
   exports: [CatsService],
 })
 export class CatsModule {}
